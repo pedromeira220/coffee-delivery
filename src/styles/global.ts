40fa4c79e8cce3ca27462c218ad8be5ddfab9ce5
Pedro Meira <petergamer8k@gmail.com>
2022-09-27 22:47:20 -0300

    chore: configuring styled components & adding default theme

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+  }
+
+  body {
+    background-color: ${props => props.theme.background};
+    color: ${props => props.theme['base-subtitle']}
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
