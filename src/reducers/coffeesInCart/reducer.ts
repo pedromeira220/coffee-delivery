2255b907461c5a3bddb3b9422bfa8f60bbc21f6f
Pedro Meira <petergamer8k@gmail.com>
2022-10-23 20:15:41 -0300

    feat: before adding the coffee to the cart, check if it is already there, if yes, only increase the amount, if not add to cart

--- src/reducers/coffeesInCart/reducer.ts
+++ src/reducers/coffeesInCart/reducer.ts
@@ -6,15 +6,45 @@ export function coffeesInCartReducer(state: ICoffee[], action: any) {
     case ActionTypes.DELETE_COFFEE_IN_CART_BY_ID:
       return state.filter(coffee => coffee.id !== action.payload.coffeeId)
     case ActionTypes.ADD_COFFEE_IN_CART:
-      const newCoffeesInCart = [
-        {
-          ...action.payload.availableCoffee,
-          amount: action.payload.amount,
-        },
-        ...state,
-      ]
+      //verificar se o café já está no carrinho, se tiver, aumentar somente o amount, se não adicionar ao carrinho
+      let amount = 0,
+        coffeeIsAlreadyInCart = false
 
+      state.forEach(coffee => {
+        console.log('coffee', coffee)
+        console.log('availableCoffee', action.payload.availableCoffee)
+
+        if (coffee.id === action.payload.availableCoffee.id) {
+          amount = action.payload.amount + coffee.amount
+          console.log(amount)
+
+          coffeeIsAlreadyInCart = true
+        }
+      })
+
+      let newCoffeesInCart: ICoffee[]
+
+      if (coffeeIsAlreadyInCart) {
+        newCoffeesInCart = state.map(coffee => {
+          if (coffee.id === action.payload.availableCoffee.id) {
+            return {
+              ...coffee,
+              amount,
+            }
+          }
+          return coffee
+        })
+      } else {
+        newCoffeesInCart = [
+          {
+            ...action.payload.availableCoffee,
+            amount: coffeeIsAlreadyInCart ? amount : action.payload.amount,
+          },
+          ...state,
+        ]
+      }
       return newCoffeesInCart
+
     case ActionTypes.UPDATE_COFFEE_IN_CART_BY_ID:
       return state.map(coffee => {
         if (coffee.id === action.payload.id) {
